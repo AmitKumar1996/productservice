0ab278ac9dbbd40149d5c33134ae65ea2b4bf26a
AmitKumar1996 <kumaramit1996.1@gmail.com>
2024-03-26 20:02:40 +0530

    How to use @PathVariable(id)

--- src/main/java/dev/amit/productservice/controllers/productController.java
+++ src/main/java/dev/amit/productservice/controllers/productController.java
@@ -1,28 +1,32 @@
 package dev.amit.productservice.controllers;
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-@RestController
+@RestController("/products")
 public class productController {
 
-    @GetMapping("/products")
+    @GetMapping
     public void getAllProducts(){
 
     }
 
-    @GetMapping("/products/{id}")
+    @GetMapping("/{id}")
     public void getProductById(){
 
     }
+    // localhost:8081/products/123
+    @GetMapping("/{id}")
+    public void deleteProductById(@PathVariable("id") Long id){
 
-    @GetMapping("/products/{id}")
-    public void deleteProductById(){
 
     }
+
+    @PostMapping
     public void createProduct(){
 
     }
+
+    @PutMapping("/{id}")
     public void createProductById(){
 
     }
