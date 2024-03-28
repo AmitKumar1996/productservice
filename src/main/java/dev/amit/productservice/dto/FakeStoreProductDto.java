3df30e267c163783841d55095ba094f2bc46e8c1
AmitKumar1996 <kumaramit1996.1@gmail.com>
2024-03-29 05:25:55 +0530

    FakeStoreProductDto

---
+++ src/main/java/dev/amit/productservice/dto/FakeStoreProductDto.java
@@ -0,0 +1,12 @@
+package dev.amit.productservice.dto;
+
+public class FakeStoreProductDto {
+    private Long id;
+   private  String title;
+   private double price;
+   private String dedcription;
+
+   private String   category;
+
+   private String image;
+}
